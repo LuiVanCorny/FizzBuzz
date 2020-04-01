f40cb9298d5005616563ee8b640651edea7a6d3f
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-01 13:42:23 +0200

    added additional tests

--- src/test/java/de/gbsschulen/FizzBuzzTest.java
+++ src/test/java/de/gbsschulen/FizzBuzzTest.java
@@ -34,4 +34,22 @@ public class FizzBuzzTest {
         String result = fizzBuzz.fizzBuzz(35);
         assertThat(result).isEqualTo("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz");
     }
+
+    @Test
+    public void testCountTo0() {
+        String result = fizzBuzz.fizzBuzz(0);
+        assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void testCountTo20(){
+        String result = fizzBuzz.fizzBuzz(20);
+        assertThat(result).isEqualTo("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz");
+    }
+
+    @Test
+    public void testCountTo30(){
+        String result = fizzBuzz.fizzBuzz(30);
+        assertThat(result).isEqualTo("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz");
+    }
 }

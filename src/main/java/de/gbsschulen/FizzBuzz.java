6b2fd21e4a22cd5a3363b96ef699b8c7b2225116
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-01 13:57:38 +0200

    Reformating code

--- src/main/java/de/gbsschulen/FizzBuzz.java
+++ src/main/java/de/gbsschulen/FizzBuzz.java
@@ -5,19 +5,19 @@ public class FizzBuzz {
     public String fizzBuzz(int zahl) {
         String resultString = "";
 
-        for(int i = 1; i <= zahl; i++){
+        for (int i = 1; i <= zahl; i++) {
 
-            if(i % 3 == 0 &&i % 5 == 0 ){
+            if (i % 3 == 0 && i % 5 == 0) {
                 resultString += "Fizz Buzz";
-            }else if(i % 3 == 0){
+            } else if (i % 3 == 0) {
                 resultString += "Fizz";
-            }else if(i % 5 == 0){
+            } else if (i % 5 == 0) {
                 resultString += "Buzz";
-            }else{
+            } else {
                 resultString += i;
             }
 
-            if(i != zahl){
+            if (i != zahl) {
                 resultString += ", ";
             }
 

e8214792a2a2864f09b34706d324452aedc61592
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-01 13:35:31 +0200

    implemented the logic for the fizzbuzz string

--- src/main/java/de/gbsschulen/FizzBuzz.java
+++ src/main/java/de/gbsschulen/FizzBuzz.java
@@ -3,7 +3,24 @@ package de.gbsschulen;
 public class FizzBuzz {
 
     public String fizzBuzz(int zahl) {
-        return "";
+        String resultString = "";
+
+        for(int i = 1; i <= zahl; i++){
+
+            if(i % 3 == 0 &&i % 5 == 0 ){
+                resultString += "Fizz Buzz";
+            }else if(i % 3 == 0){
+                resultString += "Fizz";
+            }else if(i % 5 == 0){
+                resultString += "Buzz";
+            }
+
+            if(i != zahl){
+                resultString += ", ";
+            }
+
+        }
+        return resultString;
     }
 
 }
